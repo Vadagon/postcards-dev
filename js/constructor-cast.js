a2b16efb24afd8b1f938790d7fa0c86699535f05
Vadagon <l3adagon@gmail.com>
2018-07-05 00:21:03 +0300

    little bit...

--- js/constructor-cast.js
+++ js/constructor-cast.js
@@ -1,127 +1,7 @@
 (function(){
-	var style = document.createElement('style');
-	style.innerHTML = `
-		#constructor-cast-container{
-		    height: 100%;
-		    width: 100%;
-		    overflow: hidden;
-		    display: block;
-		    position: absolute;
-		    top: 0;
-		    left: 0;
-		}
-		#constructor-cast-main{
-			background-color: blue; 
-			position: absolute; 
-			vertical-align: middle; 
-			border-style: none;
-
-			background-position: center:
-			background-size: cover;
-			font-size: 16px;
-			text-align: left;
-		}
-		#constructor-cast-fake{
-			font-size: 16px;
-			width: 0;
-			position: absolute; 
-			top: 0;
-			left: 0;
-			height: 0;
-		}
-		.text-insertation-tool{
-			top: 25px;
-			left: 25px;
-			padding: 5px;
-			border: 1px solid black;
-			cursor: pointer;
-			display: inline-block;
-		}
-		.box {
-		  border: 2px solid black;
-		  width: 100px;
-		  height: 100px;
-		  background-color: green;
-		  margin: 27px;
-		  position: relative;
-		}
-
-		.ui-rotatable-handle {
-		  background: url("https://cdn.jsdelivr.net/jquery.ui.rotatable/1.0.1/rotate.png");
-		  background-repeat: no-repeat;
-		  background-size: 100% 100%;
-		  height: 25px;
-		  width: 25px;
-		  position: absolute;
-		}
-
-		.ui-rotatable-handle-sw {
-		  bottom: -27px;
-		  left: -27px;
-		}
-
-		.ui-rotatable-handle-nw {
-		  top: -27px;
-		  left: -27px;
-		}
-
-		.ui-rotatable-handle-se {
-		  bottom: -27px;
-		  right: -27px;
-		}
-
-		.ui-rotatable-handle-ne {
-		  top: -27px;
-		  right: -27px;
-		}
-		.text-insertation-tool{
-			top: 50px;
-			left: 50px;
-			padding: 10px;
-			background: none;
-			box-sizing: border-box;
-			position: relative;
-		}
-		.text-insertation-tool .insertation-content{
-			// resize: both;
-			width: 100%;
-			height: 100%;
-			display: inline-block;
-			overflow: hidden;
-		}
-		.text-insertation-tool > div:not(.ui-resizable-handle){
-		    background-position: 0 center;
-		    background-repeat: no-repeat;
-		    margin-right: 20px;
-		    background-image: url(img/icons/cancel.png);
-		    background-size: contain;
-		    height: 25px;
-		    width: 25px;
-		    position: absolute;
-		}
-		.text-insertation-tool div.insertation-moving-tool{
-			top: -20px;
-		    left: -20px; 
-		}
-		.text-insertation-tool .ui-rotatable-handle.ui-draggable{
-			bottom: -20px;
-		    left: -20px;
-		}
-		.text-insertation-tool div.insertation-editing-tool{
-			top: -20px;
-		    right: -20px;
-		}
-		.text-insertation-tool:not(.editable){
-			border: none;
-		}
-		.text-insertation-tool:not(.editable) > div{
-			display: none;
-		}
-		.text-insertation-tool:not(.editable) .insertation-content {
-			// resize: none;
-		}
-
-	`;
+	var style = document.createElement('link');
+	style.rel = "stylesheet"
+	style.href = "js/constructor/constructor.css"
 	document.body.appendChild(style);
 
 	var c = {
@@ -136,13 +16,31 @@
 			$('.creation__tool .creation__gallery_img').click(function(){
 				c.blur(c.fake.find('*'))
 				c.tools[c.activeTool]($(this).data('bg'))
+				c.showAdditionalSettings(c.activeTool);
 			})
 			c.fake.on('click', '.text-insertation-tool .insertation-content', function(){
 				console.log($(this).parent().hasClass('editable'))
 				if(!$(this).parent().hasClass('editable')){
 					c.blur(c.fake.find('*'))
 					c.focus($(this).parent())
+					c.showAdditionalSettings($(this).parent().data("creation__tool"));
 				}
+			})
+			$('#constructor-settings-text :input').change(function(e){
+				var css = {};
+				switch ($(this).attr('type')){
+					case 'select':
+						css[$(this).data('option')] = $(this).val()
+						c.fake.find('.editable').css(css);
+						break;
+					case 'radio':
+						css[$(this).closest('fieldset').data('option')] = $(this).val()
+						c.fake.find('.editable').css(css);
+						break;
+					default:
+						console.log(1);
+				}
+				console.log(css)
 			})
 			c.c.click(function(){
 				c.blur(c.fake.find('*'))
@@ -154,7 +52,7 @@
 			},
 			img: function(e){
 				console.log(e)
-				$(`<div class="text-insertation-tool editable">
+				$(`<div class="text-insertation-tool editable" data-creation__tool="img">
 					<img src="${e}" class="insertation-content">
 					<div class="insertation-moving-tool"></div>
 					<div class="insertation-editing-tool"></div>
@@ -168,20 +66,22 @@
 				})
 				.rotatable()
 				.resizable({
-					containment: "#constructor-cast-main"
+					containment: "#constructor-cast-main",
+
 				});
 			},
 			bg: function(e){
 				c.c.css({backgroundImage: 'url('+e+')'})
 			},
 			text: function(){
-				$(`<div class="text-insertation-tool editable">
-					<content contenteditable="true" class="insertation-content">23456</content>
+				$(`<div class="text-insertation-tool editable" data-creation__tool="text">
+					<font contenteditable="true" class="insertation-content">sample text</font>
 					<div class="insertation-moving-tool"></div>
 					<div class="insertation-editing-tool"></div>
 					</div>`).appendTo(c.fake)
 				.resizable({
-					containment: "#constructor-cast-main"
+					containment: "#constructor-cast-main",
+					handles: 'e, w'
 				})
 				.draggable({
 					containment: "#constructor-cast-main",
@@ -194,12 +94,24 @@
 		},
 		blur: function(e){
 			e.removeClass('editable').find('.insertation-content').attr('contenteditable', 'false')
+			c.showAdditionalSettings()
 		},
 		focus: function(e){
 			e.addClass('editable').find('.insertation-content').attr('contenteditable', 'true')
 		},
 		toolSwitching: function(){
 
+		},
+		showAdditionalSettings: function(e){
+			$('.constructor-additional-ui').hide();
+			switch (e){
+				case 'text':
+					// start creating additional ui
+					$('#constructor-settings-'+e).show()
+					break;
+				default:
+					console.log('brakAll')	
+			}
 		},
 		reposition: function(){
 			c.width = $('.creation_playground img').width()
@@ -209,10 +121,69 @@
 		},
 		init: function(){
 			c.reposition()
-			$('.creation_playground').append(`<div id="constructor-cast-container"><div id="constructor-cast-main" style="height: ${c.height}px; width: ${c.width}px; left: ${c.position.left}px; top: ${c.position.top}px;"></div><div id="constructor-cast-fake"></div>`);
+			$('.creation_playground').append(`
+				<div id="constructor-cast-container">
+					<div id="constructor-cast-main" style="height: ${c.height}px; width: ${c.width}px; left: ${c.position.left}px; top: ${c.position.top}px;"></div>
+					<div id="constructor-cast-fake"></div>
+					<div class="constructor-additional-ui" id="constructor-settings-text">
+						<select type="select" data-option="font-family">
+							<option value="Georgia, serif">Georgia, serif</option>
+							<option value="Verdana, Geneva, sans-serif">Verdana, Geneva, sans-serif</option>
+							<option value="'Courier New', Courier, monospace">'Courier New', Courier, monospace</option>
+						</select>
+						<select type="select" data-option="font-size">
+							<option value="16px">16px</ption>
+							<option value="11px">11px</option>
+							<option value="32px">32px</option>
+						</select>
+
+						<fieldset data-option="text-align">
+						    <legend>Text alignment</legend>
+
+						    <div>
+						        <input type="radio" name="text-align" value="left" id="huey"  checked>
+						        <label for="huey">Left</label>
+						    </div>
+
+						    <div>
+						        <input type="radio" name="text-align" value="center" id="dewey">
+						        <label for="dewey">Center</label>
+						    </div>
+
+						    <div>
+						        <input type="radio" name="text-align" value="right" id="louie">
+						        <label for="louie">Right</label>
+						    </div>
+
+						</fieldset>
+
+						<fieldset data-option="color">
+						    <legend>Color</legend>
+
+						    <div>
+						        <input type="radio" name="color" value="black" id="huey1" checked>
+						        <label for="huey1">Black</label>
+						    </div>
+
+						    <div>
+						        <input type="radio" name="color" value="red" id="dewey1">
+						        <label for="dewey1">Red</label>
+						    </div>
+
+						    <div>
+						        <input type="radio" name="color" value="green" id="louie1">
+						        <label for="louie1">Green</label>
+						    </div>
+
+						</fieldset>
+
+					</div>
+				</div>`);
 			c.c = $('#constructor-cast-main').eq(0)
 			c.fake = $('#constructor-cast-fake').eq(0)
-			c.tools.img('img/main_bg.png')
+
+			c.tools.text('img/main_bg.png')
+			c.showAdditionalSettings('text')
 		}
 	}
 
